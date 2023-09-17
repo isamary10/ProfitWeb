556308e79f994b2a24ed276777ee171a47b57c4c
Isa Mary <isa.kusuki@gmail.com>
2023-09-17 20:27:12 -0300

    error

---
+++ src/app/simulacoes/erro.jsx
@@ -0,0 +1,25 @@
+'use client' // Error components must be Client Components
+
+import Button from '@/components/Button'
+import { useEffect } from 'react'
+ 
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className='flex justify-center items-center p-10 flex-col'>
+      <h2>Um erro aconteceu.</h2>
+      <p>{error.message}</p>
+      <div className='flex gap-3'>
+        <Button variant='secundary' href="/">
+          voltar para home
+        </Button>
+        <Button element='button' onClick={ () => reset() }>
+          Tentar novamente
+        </Button>
+      </div>
+    </div>
+  )
+}
